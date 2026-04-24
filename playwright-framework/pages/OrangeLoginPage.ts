f3f58e2c950cc06340384372908bb220bfdc3f26
Om Gutty <om.gutty@gmail.com>
2026-04-24 10:36:55 +0530

    ignore

--- playwright-framework/pages/OrangeLoginPage.ts
+++ playwright-framework/pages/OrangeLoginPage.ts
@@ -7,7 +7,8 @@ export class OrangeLoginPage extends BasePage{
     readonly username:Locator;
     readonly password:Locator;
     readonly loginbutton:Locator;
-    
+    readonly footerlinks:Locator;
+    readonly linkedinlink:Locator;
 
 
     constructor(page:Page){
@@ -17,16 +18,22 @@ export class OrangeLoginPage extends BasePage{
         this.username= page.getByRole('textbox',{name:'username'});
         this.password= page.getByRole('textbox',{name:'password'});
         this.loginbutton=page.getByRole('button',{name:' Login '})
+        this.footerlinks= page.locator(`//div[@class='orangehrm-login-footer-sm']`);
+        this.linkedinlink=page.locator(`//div[@class='orangehrm-login-footer-sm']/a[contains(@href,'linked')]`);
     }
 
     async login(user: string, pass: string){
-        await this.username.fill(user);
-        await this.password.fill(pass);
-        await this.loginbutton.click();
+        // await this.username.fill(user);
+        // await this.password.fill(pass);
+        // await this.loginbutton.click();
         
         //using BasePage methods
-        // await this.fillmethod(this.username, user);
-        // await this.fillmethod(this.password, pass);
-        // await this.clickmethod(this.loginbutton);
+        await this.fill(this.username,user)
+        await this.fill(this.password, pass);
+        await this.click(this.loginbutton);   
+    }
+
+    async clickonLinkedinFooterLink(){
+        this.linkedinlink.click();
     }
 }
